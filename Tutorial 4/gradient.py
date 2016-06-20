0358ae11702ed90a1e868f3923e667ea572da281
Florian Lindner <florian.lindner@ipvs.uni-stuttgart.de>
2016-06-20 14:41:36 +0200

    Add timings and some doc.

--- Tutorial 4/gradient.py
+++ Tutorial 4/gradient.py
@@ -1,10 +1,10 @@
 import numpy as np
 import scipy.linalg
-import timeit
+import time
 import matplotlib.pyplot as plt
 
 tolerance = 1e-3
-max_iter = 500
+max_iter = 1000
 N = 40
 cg_residuals = []
 gr_residuals = []
@@ -13,8 +13,8 @@ gr_residuals = []
 def CG(A, x, b):
     """ Preconditioned Conjugate Gradients algorithm, see p. 94 in script. """
     iter = 1
-    # P = np.identity(N) # Preconditioner
-    P = np.linalg.inv(np.diagflat(np.diag(A))) # Jacobi Preconditoner P = diag(A)^-1
+    P = np.identity(N) # No Preconditioner
+    # P = np.linalg.inv(np.diagflat(np.diag(A))) # Jacobi Preconditoner P = diag(A)^-1
 
     r0 = b - A @ x # For the first iteration search direction is like with gradient methods
     h0 = P @ r0
@@ -30,7 +30,7 @@ def CG(A, x, b):
             print("CG: Converged after %i iterations." % iter)
             break
         elif iter > max_iter:
-            print("CG: Maximum iterations reached.")
+            print("CG: Maximum iterations reached. Last residual: %f" % np.linalg.norm(b - A @ x))
             break
         beta = (r1 @ h1) / (r0 @ h0)
         p = h1 + beta * p # Search direction
@@ -45,14 +45,14 @@ def gradient(A, x, b):
     r = b - A @ x # Residual
     while True:
         a = (r @ np.transpose(r)) / (np.transpose(r) @ A @ r) # Step size
-        x = x + a * r # New solution = old solution + search direction * residual
+        x = x + a * r # New solution = old solution + step size * residual
         r = b - A @ x # New residual
         gr_residuals.append(np.linalg.norm(r))
         if np.linalg.norm(r) < tolerance:
             print("Gradient: Converged after %i iterations." % iter)
             break
         elif iter > max_iter:
-            print("Gradient: Maximum iterations reached.")
+            print("Gradient: Maximum iterations reached. Last residual: %f" % np.linalg.norm(b - A @ x))
             break
         iter += 1
     return x
@@ -66,10 +66,22 @@ b = np.array(range(1, N+1))
 ev = scipy.linalg.eig(A)[0]
 if np.any(np.real(ev) < 0):
     print("Negative Eigenvalues, matrix is not semi-positive definite!")
-    
+
+t = time.perf_counter()
 sp_solution = scipy.linalg.solve(A,b)
+t_sp = time.perf_counter()-t
+
+t = time.perf_counter()
 cg_solution = CG(A, x0, b)
+t_cg =  time.perf_counter()-t
+
+t = time.perf_counter()
 gr_solution = gradient(A, x0, b)
+t_gr = time.perf_counter()-t
+
+print("Time SP: ", t_sp)
+print("Time CG: ", t_cg)
+print("Time GR: ", t_gr)
 
 print("Delta Conjugate Gradient = ", np.linalg.norm(cg_solution - sp_solution))
 print("Delta Simple Gradient = ", np.linalg.norm(gr_solution - sp_solution))
